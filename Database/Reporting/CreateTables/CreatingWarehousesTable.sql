d81db50aab6a382c6614e1162f4c27769a4cd4b7
DeryGermann <germanndery@gmail.com>
2023-06-27 20:05:25 -0500

    Finished Warehouse Table Creation Script

--- Database/Reporting/CreateTables/CreatingWarehousesTable.sql
+++ Database/Reporting/CreateTables/CreatingWarehousesTable.sql
@@ -2,3 +2,22 @@ USE NuvemProjectExercise;
 GO
 
 -- Needs to be related to the Deliveries table
+IF EXISTS (SELECT name 
+    FROM sys.tables
+    WHERE name = 'Warehouses')
+
+    BEGIN
+        DROP TABLE Warehouses
+    END
+
+IF NOT EXISTS (SELECT name 
+    FROM sys.tables
+    WHERE name = 'Warehouses')
+
+    BEGIN
+        CREATE TABLE Warehouses (
+            WarehouseID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+            WarehouseName nvarchar(50),
+            Address nvarchar(500)
+        );    
+    END   
