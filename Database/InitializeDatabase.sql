955bf5e52afe956f21d40e2db59412886064e727
DeryGermann <germanndery@gmail.com>
2023-06-27 19:01:46 -0500

    Spring Cleaning
    Cleaned up the SQL scripts to create the database along with populating the Pharmacy table.

--- Database/InitializeDatabase.sql
+++ Database/InitializeDatabase.sql
@@ -1,20 +1,24 @@
 USE master;
 GO
 
+-- Check if DB exists
 IF EXISTS (
-      SELECT name
-      FROM sys.databases
-      WHERE name = N'NuvemProjectExercise'
-      )
+    SELECT name
+    FROM sys.databases
+    WHERE name = 'NuvemProjectExercise')
+    
     BEGIN
-        ALTER DATABASE NuvemProjectExercise SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+        -- Set database to single_user to remove any lingering connection
+        ALTER DATABASE NuvemProjectExercise SET SINGLE_USER;
         DROP DATABASE NuvemProjectExercise;
     END
 GO
 
 IF NOT EXISTS (
-      SELECT name
-      FROM sys.databases
-      WHERE name = N'NuvemProjectExercise'
-      )
-   CREATE DATABASE [NuvemProjectExercise];
+    SELECT name
+    FROM sys.databases
+    WHERE name = 'NuvemProjectExercise')
+
+    BEGIN
+        CREATE DATABASE [NuvemProjectExercise];
+    END
